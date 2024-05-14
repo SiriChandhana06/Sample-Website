9cdb0d2e054511de0d4c19fe9d1827acc0f301c8
SiriChandhana06 <sirisiri3006@gmail.com>
2024-05-14 09:18:48 +0530

    react

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,38 +5,39 @@ import { Helmet } from 'react-helmet';
 
 
 const Home = () => {
-  return (
-    <div>
-      <Helmet>
-        <title>Home</title>
-        <meta name="title" content="Home" />
-        <meta name="description" content="Home page in Website" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://sample-website-hello.vercel.app/" />
-        <meta property="og:title" content="Home" />
-        <meta property="og:description" content="Home page in Website" />
-        <meta property="og:image"
-          content="https://metatags.io/images/meta-tags.png" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://sample-website-hello.vercel.app/" />
-        <meta property="twitter:title" content="Home" />
-        <meta property="twitter:description" content="Home page in Website" />
-        <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+    return (
+        <div className='bg-gray-900 h-screen'>
+        <Helmet>
+          <title>Home</title>
+          <meta name="title" content="Home" />
+          <meta name="description" content="Home page in Website" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://sample-website-hello.vercel.app/" />
+          <meta property="og:title" content="Home" />
+          <meta property="og:description" content="Home page in Website" />
+          <meta property="og:image"
+            content="https://metatags.io/images/meta-tags.png" />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://sample-website-hello.vercel.app/" />
+          <meta property="twitter:title" content="Home" />
+          <meta property="twitter:description" content="Home page in Website" />
+          <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
 
-      </Helmet>
-      <div className='bg-gray-900 h-screen'>
-        <Nav />
-        <div className='grid grid-cols-2 px-20'>
-          <div className='h-4/5 w-4/5 p-4'>
-            <img src={illustrate} alt="illustrate" />
+        </Helmet>
+        <div>
+          <Nav />
           </div>
-          <div className='pt-24'>
-            <h1 className='text-white font-medium text-justify'>Welcome to Sample Website, a dynamic online platform that seamlessly integrates elements from Web 1.0, Web 2.0, and Web 3.0 eras, offering a multifaceted digital experience. Explore the nostalgic charm of Web 1.0 with our collection of static web pages, reminiscent of the early days of the internet, providing informative content and resources. Immerse yourself in the interactive world of Web 2.0 as you engage with user-generated content, participate in discussions, and contribute to our vibrant online community. Additionally, discover the cutting-edge innovations of Web 3.0, where intelligent algorithms, decentralized technologies, and personalized experiences redefine the internet landscape. Whether you're seeking timeless information, social interactions, or futuristic advancements, sample website caters to all audiences, blending the best of past, present, and future web technologies into a unified digital platform.</h1>
+          <div className='grid grid-cols-2 px-20'>
+            <div className='h-4/5 w-4/5 p-4'>
+              <img src={illustrate} alt="illustrate" />
+            </div>
+            <div className='pt-24'>
+              <h1 className='text-white font-medium text-justify'>Welcome to Sample Website, a dynamic online platform that seamlessly integrates elements from Web 1.0, Web 2.0, and Web 3.0 eras, offering a multifaceted digital experience. Explore the nostalgic charm of Web 1.0 with our collection of static web pages, reminiscent of the early days of the internet, providing informative content and resources. Immerse yourself in the interactive world of Web 2.0 as you engage with user-generated content, participate in discussions, and contribute to our vibrant online community. Additionally, discover the cutting-edge innovations of Web 3.0, where intelligent algorithms, decentralized technologies, and personalized experiences redefine the internet landscape. Whether you're seeking timeless information, social interactions, or futuristic advancements, sample website caters to all audiences, blending the best of past, present, and future web technologies into a unified digital platform.</h1>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  )
+    );
+  
 }
 
 export default Home;
--- src/Pages/Web2.jsx
+++ src/Pages/Web2.jsx
@@ -5,9 +5,9 @@ import Nav from '../Components/Nav';
 
 const Web2 = () => {
   return (
-    <div>
+    <div className='bg-gray-900 h-screen '>
       <Helmet>
-                {/* <title>Web 2</title>
+        {/* <title>Web 2</title>
                 <meta name="description" content="This is the Web 2.0 page of my website." />
                 <meta property="og:title" content="WEB 2.0 Page" />
                 <meta property="og:description" content="This is the Web 2.0 page of my website." />
@@ -17,7 +17,7 @@ const Web2 = () => {
                 <meta property="og:url" content="https://sample-website-seven.vercel.app/web2" />
                 <meta property="og:type" content="website"/> */}
 
-<title>Web 2</title>
+        <title>Web 2</title>
         <meta name="title" content="Web 2" />
         <meta name="description" content="Home page in Website" />
         <meta property="og:type" content="website" />
@@ -32,20 +32,20 @@ const Web2 = () => {
         <meta property="twitter:description" content="Home page in Website" />
         <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
 
-        </Helmet>
-    <div className='bg-gray-900 h-screen '>
-      <Nav/>
-      <div className='grid grid-cols-2 px-10 py-20'> 
+      </Helmet>
       <div>
-        <div className='pt-10 pl-20' >
-          <img src={w2} alt='web2'/>
-        </div>
+        <Nav />
       </div>
+      <div className='grid grid-cols-2 px-10 py-20'>
+        <div>
+          <div className='pt-10 pl-20' >
+            <img src={w2} alt='web2' />
+          </div>
+        </div>
         <div >
           <h1 className='text-white text-justify text-lg'>Web 2.0 marks a pivotal shift in the internet landscape, transforming static web pages into dynamic, interactive platforms. Emerging in the early 2000s, this era emphasizes user-generated content and collaboration. Social media networks like Facebook and Twitter epitomize Web 2.0, enabling users to engage with content through likes, shares, and comments, fostering real-time communication and connection. Dynamic web applications, powered by technologies like AJAX, provide seamless and responsive user experiences, blurring the lines between web and desktop applications. Additionally, the semantic web enhances data interoperability, making information more accessible and interpretable by machines, while mobile and cloud computing ensure ubiquitous access to online content across devices. Web 2.0 has democratized content creation, empowering users to shape the digital discourse and participate in a global community, ultimately redefining the internet as a collaborative and interactive platform.</h1>
         </div>
       </div>
-    </div>
     </div>
   )
 }
