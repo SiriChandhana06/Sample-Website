a231514a438c68967493297da9dcec5360beda22
SiriChandhana06 <sirisiri3006@gmail.com>
2024-05-15 21:15:18 +0530

    react

--- src/Pages/Web2.jsx
+++ src/Pages/Web2.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+// import { Helmet } from 'react-helmet';
 import w2 from '../Assets/web2.webp';
 import Nav from '../Components/Nav';
 
